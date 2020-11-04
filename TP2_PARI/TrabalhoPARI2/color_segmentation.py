139b340ae35b252c51ef9fc35c4762f7c6ce335f
GuilhomenAguiar <guilherme.aguiar@ua.pt>
2020-11-04 15:14:57 +0000

    Falta guardar em formato json

--- TP2_PARI/TrabalhoPARI2/color_segmentation.py
+++ TP2_PARI/TrabalhoPARI2/color_segmentation.py
@@ -2,10 +2,19 @@
 import cv2
 
 
+def nothing(x):
+    pass
+
 def main():
     # define a video capture object
     vid = cv2.VideoCapture(0)
-    colorwindow = cv2.namedWindow("Color Segmentation")
+    video = cv2.namedWindow('mask')
+    cv2.createTrackbar('Bmin', 'mask', 0, 255, nothing)
+    cv2.createTrackbar('Bmax', 'mask', 255, 255, nothing)
+    cv2.createTrackbar('Gmin', 'mask', 0, 255, nothing)
+    cv2.createTrackbar('Gmax', 'mask', 255, 255, nothing)
+    cv2.createTrackbar('Rmin', 'mask', 0, 255, nothing)
+    cv2.createTrackbar('Rmax', 'mask', 255, 255, nothing)
 
     while True:
 
@@ -14,7 +23,22 @@ def main():
         _, frame = vid.read()
 
         # Display the resulting frame
-        cv2.imshow(colorwindow, frame)
+        cv2.imshow('video color', frame)
+
+        l_b = cv2.getTrackbarPos('Bmin', 'mask')
+        h_b = cv2.getTrackbarPos('Bmax', 'mask')
+        l_g = cv2.getTrackbarPos('Gmin', 'mask')
+        h_g = cv2.getTrackbarPos('Gmax', 'mask')
+        l_r = cv2.getTrackbarPos('Rmin', 'mask')
+        h_r = cv2.getTrackbarPos('Rmax', 'mask')
+
+        lower_color = (l_b, l_g, l_r)
+        upper_color = (h_b, h_g, h_r)
+
+        mask = cv2.inRange(frame, lower_color, upper_color)
+
+        cv2.imshow('mask', mask)
+
 
         # the 'q' button is set as the
         # quitting button you may use any
